35fd3d9235eeee0f0346632a5a765d8897b517dd
Tokimikichika <ahileses@mail.ru>
2024-11-05 18:59:12 +0300

    попытка настроить билд

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js', 
+    chunkFilename: '[name].[contenthash].js', 
+    clean: true, 
   },
   module: {
     rules: [
@@ -15,21 +20,28 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env', '@babel/preset-react'],
+            cacheDirectory: true,
           },
         },
       },
       {
-        test: /\.css$/,            
+        test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|jpe?g|gif)$/i, 
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset',
+        generator: {
+          filename: 'assets/images/[name].[hash][ext]',
+        },
         use: [
           {
-            loader: 'file-loader',
+            loader: 'image-webpack-loader', 
             options: {
-              name: '[name].[hash].[ext]',
-              outputPath: 'assets/images',
+              mozjpeg: { progressive: true },
+              optipng: { enabled: true },
+              pngquant: { quality: [0.65, 0.90], speed: 4 },
+              gifsicle: { interlaced: false },
             },
           },
         ],
@@ -39,5 +51,34 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  optimization: {
+    splitChunks: {
+      chunks: 'all', 
+      maxInitialRequests: 10, 
+      maxAsyncRequests: 10,
+      minSize: 20 * 1024, 
+    },
+    runtimeChunk: 'single', 
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true, 
+          },
+        },
+        extractComments: false,
+        parallel: true, 
+      }),
+      new CssMinimizerPlugin(),
+    ],
+  },
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static', 
+      openAnalyzer: false, 
+      reportFilename: 'bundle-report.html',
+    }),
+  ],
   mode: 'production',
 };
