032a2ae5c6e79b132dbcad0e8f30210a592569a8
Tokimikichika <ahileses@mail.ru>
2024-12-14 16:18:16 +0300

    deploy

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -5,9 +5,9 @@ const apiPath = '/api/v1';
 const routes = {
   loginPath: () => [apiPath, 'login'].join('/'),
   signupPath: () => [apiPath, 'signup'].join('/'),
-  chatPagePath: () => `/`,
-  loginPagePath: () => `/login`,
-  signupPagePath: () => `/signup`,
+  chatPagePath: () => '/',
+  loginPagePath: () => '/login',
+  signupPagePath: () => '/signup',
 };
 
 export default routes;
